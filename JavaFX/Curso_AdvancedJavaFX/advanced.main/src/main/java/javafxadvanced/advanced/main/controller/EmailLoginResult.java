a03865860365f250d5b5c9cf24b4e7802fc4b88c
alexandreBez <lucasbez14@outlook.com>
2022-02-06 11:25:04 -0300

    javafx

--- JavaFX/Curso_AdvancedJavaFX/advanced.main/src/main/java/javafxadvanced/advanced/main/controller/EmailLoginResult.java
+++
@@ -1,8 +0,0 @@
-package javafxadvanced.advanced.main.controller;
-
-public enum EmailLoginResult {
-    SUCESS,
-    FAILED_BY_CREDENTIALS,
-    FAILED_BY_NETWORK,
-    FAILED_BY_UNEXPECTED_ERROR
-}
